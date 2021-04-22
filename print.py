357cd75c2e654b1a696941bb20a9f7ac45cf61fd
joydeep2899 <joydeep779@gmail.com>
2021-04-22 16:45:42 +0530

    printing pattern

---
+++ print.py
@@ -0,0 +1,11 @@
+#print("HELLO WORLD",sep="\n")
+
+
+
+for i in range(10):
+     for j in range(10):
+                if(j in range((10//2)-1,(10//2)+i)     ):
+                     print("*",end="")
+                else:
+                     print(" ",end="")     
+     print("\n")                
