6ca97bdcf877eb8de96dd6918f95533e202f90d8
joydeep2899 <joydeep779@gmail.com>
2021-04-22 17:01:02 +0530

    added function print triangle

--- print.py
+++ print.py
@@ -1,12 +1,29 @@
 #print("HELLO WORLD",sep="\n")
 
 
+def print_trapezium():
+             print("printing a trapzeium")
+             for i in range(10):
+                   for j in range(10):
+                        if(j in range((10//2)-1,(10//2)+i)     ):
+                           print("*",end="")
+                        else:
+                          print(" ",end="")     
+                   print("\n")                
 
-for i in range(10):
-     for j in range(10):
-                if(j in range((10//2)-1,(10//2)+i)     ):
-                     print("*",end="")
-                else:
-                     print(" ",end="")     
-     print("\n")                
-     
+
+
+def print_triangle(n):
+           print("printing a triangle")
+           for i in range(n):
+                for j in range(i):
+                        print("*",end="")
+                print("\n")
+                                        
+               
+           
+      
+if __name__=="__main__":
+              print_trapezium()
+              print_triangle(10) 
+              
